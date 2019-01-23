0f902b10d58a4fe6ba3dc9b0a62bc9738cc590ab
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-01-23 17:24:26 +0100

    add AC and back to the reg expression add about section fixed display operators

--- src/App.js
+++ src/App.js
@@ -3,70 +3,76 @@ import Buttons from "./component/Buttons";
 
 import "./App.css";
 
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
       formula: "0",
       input: "0",
-      buffer:''
     };
   }
-  calc(formula){
-    formula = formula.replace(/(.)([//*+/-])$/,'$1')
+  calc(formula) {
+    formula = formula.replace(/(.)([//*+/-])$/, "$1").replace(/×/g,'*').replace(/÷/g,'/');
     let result = Math.round(10000000000 * eval(formula)) / 10000000000;
-    return{
-      formula: formula + '=' + result,
+    formula = formula.replace(/[*]/g,'×').replace(/[//]/g,'÷');
+    return {
+      formula: formula + "=" + result,
       input: result
-    }
+    };
   }
- 
+
   handleClick = e => {
     const val = e.target.dataset.val;
     let formula = this.state.formula;
     let input = this.state.input;
 
-    if (val === "clear") {
-      this.setState({
-        formula: '0',
-        input: '0'
-      });
+    if(input.length >10 && /\d/.test(val)){
       return;
     }
-    if (val === "back" && !/=/.test(formula)) {
-      this.setState({
-        formula: formula.length === 1 ? '0' : formula.slice(0,-1),
-        input: input.length === 1 ? '0' : input.slice(0,-1)
-      });
-      return;
-    }
-    if(val === 'equals'){
-      if(/=/g.test(formula)){
+
+    if (val === "equals") {
+      if (/=/g.test(formula)) {
         return;
       }
-      this.setState(this.calc(formula))
-      return 
-    }
-    if(/[\d//*+./-]/.test(val)){
-      if(/=/.test(formula) && /[\d.]/.test(val)){
-        input = '0'
-        formula = '0'
-      }
-      formula = (formula + val).replace(/(.+=)(.+)/, '$2').replace(/^[0//*+]+/,'').replace(/^[.]/,'0.').replace(/([//*+/-])[.]/,'$1'+'0.').replace(/(\D)(\D)/,'$2').replace(/(\d+[.]\d+)[.]/,'$1').replace(/(.+=)(.+)/, '$2');  
-      input = (input + val).replace(/^[0]/,'').replace(/^[.]/,'0.').replace(/(.*)([^.\d])/,'$2').replace(/(\D)*(.)/,'$2').replace(/[.]+/g,'.').replace(/(\d+[.]\d+)[.]/,'$1').replace(/^[.]/,'0.');
-      this.setState({
-        formula: formula === '' ? '0' : formula,
-        input
-      });
+      this.setState(this.calc(formula));
       return;
     }
-    
-   
+
+    if (/=/.test(formula) && /[\d.]/.test(val)) {
+      input = "0";
+      formula = "0";
+    }
+    formula = (formula + val)
+      .replace(/.*clear/, "")
+      .replace(/(.*)(.)back/, "$1")
+      .replace(/(.+=)(.+)/, "$2")
+      .replace(/^[0//*+]+/, "")
+      .replace(/^[.]/, "0.")
+      .replace(/([//*+/-])[.]/, "$1" + "0.")
+      .replace(/(\D)(\D)/, "$2")
+      .replace(/(\d+[.]\d+)[.]/, "$1")
+      .replace(/(.+=)(.+)/, "$2");
+
+    input = (input + val)
+      .replace(/.*clear/, "")
+      .replace(/(.*)(.)back/, "$1")
+      .replace(/^[0]/, "")
+      .replace(/^[.]/, "0.")
+      .replace(/(.*)([^.\d])/, "$2")
+      .replace(/(\D)*(.)/, "$2")
+      .replace(/[.]+/g, ".")
+      .replace(/(\d+[.]\d+)[.]/, "$1")
+      .replace(/^[.]/, "0.");
+    this.setState({
+      formula: formula === "" ? "0" : formula,
+      input: input === "" ? "0" : input
+    });
+    return;
   };
   render() {
     return (
       <div className="container">
+        <div className="about">Coded by <a href="https://github.com/yakhousam/Calculator-React">yakhousam</a></div>
         <div id="formula">{this.state.formula}</div>
         <div id="display">{this.state.input}</div>
         <Buttons onClick={this.handleClick} />
--- src/component/Buttons.js
+++ src/component/Buttons.js
@@ -2,27 +2,27 @@ import React from 'react'
 
 const arrayButtons = [
     {id:'clear', content:'AC' , data:'clear'},
-    {id:'back', content:'⇦' , data:'back'},
-    {id:'divide', content:'÷' , data:'/'},    
+    {id:'back', content:'⌫' , data:'back'},
+    {id:'divide', content:'÷' , data:'÷'},    
     
     {id:'seven', content:'7' , data:'7'},
     {id:'eight', content:'8' , data:'8'},
     {id:'nine', content:'9' , data:'9'},
-    {id:'multiply', content:'×' , data:'*'},
+    {id:'multiply', content:'×' , data:'×'},
     
     {id:'four', content:'4' , data:'4'},
     {id:'five', content:'5' , data:'5'},
     {id:'six', content:'6' , data:'6'},
-    {id:'subtract', content:'⁻' , data:'-'},
+    {id:'subtract', content:'-' , data:'-'},
    
     {id:'one', content:'1' , data:'1'},
     {id:'two', content:'2' , data:'2'},
     {id:'three', content:'3' , data:'3'},
-    {id:'add', content:'₊' , data:'+'},
+    {id:'add', content:'+' , data:'+'},
         
     {id:'zero', content:'0' , data:'0'},
     {id:'decimal', content:'.' , data:'.'},
-    {id:'equals', content:'⁼' , data:'equals'},
+    {id:'equals', content:'=' , data:'equals'},
 ]
 
 const Buttons = (props) => {
