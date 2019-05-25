96356634a2c7a1f1267d2c291dd48e5fe54726b1
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-05-25 16:16:25 +0100

    Updates

---
+++ precache-manifest.5338fc60b431606237d8f6e01f4e03df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5318293bc3e9106729e",
+    "url": "/Calculator-React/static/js/runtime~main.c5318293.js"
+  },
+  {
+    "revision": "a5fb69f09f7dcf965328",
+    "url": "/Calculator-React/static/js/main.a5fb69f0.chunk.js"
+  },
+  {
+    "revision": "b937c28302eccc458065",
+    "url": "/Calculator-React/static/js/1.b937c283.chunk.js"
+  },
+  {
+    "revision": "a5fb69f09f7dcf965328",
+    "url": "/Calculator-React/static/css/main.3828e63f.chunk.css"
+  },
+  {
+    "revision": "509228c0a7b59f903ade933c2c5aa8f2",
+    "url": "/Calculator-React/index.html"
+  }
+];
